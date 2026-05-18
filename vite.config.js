063ed680c823f1e6de9651b2026ce56563dbc3fa
xiaowu <linjun@xiaowudeMacBook-Air.local>
2026-05-18 16:47:48 +0800

    fix vite base path

--- vite.config.js
+++ vite.config.js
@@ -23,7 +23,7 @@ export default defineConfig({
       ],
     }),
   ],
-  base:'/UnifyingKeyframesSemantics',
+  base:'/UnifyingKeyframesSemantics/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
